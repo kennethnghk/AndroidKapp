13524f0062793b44580bed97f7cf6127349e4ab5
Kenneth Ng <ng.tszcheong@gmail.com>
2017-03-09 21:18:02 +0800

    Temp work of fragment interaction

--- app/src/main/java/com/kapp/androidkapp/ArticleFragment.java
+++ app/src/main/java/com/kapp/androidkapp/ArticleFragment.java
@@ -1,19 +1,52 @@
 package com.kapp.androidkapp;
 
-import android.os.Bundle;
+import android.app.Activity;
 import android.support.v4.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ArticleFragment extends Fragment {
 
     public static final String ARG_POSITION = "ARG_POSITION";
+
+    OnArticleSelectedListener mCallback;
+
+    // Container Activity must implement this interface
+    public interface OnArticleSelectedListener {
+        public void onArticleSelected(int position);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnArticleSelectedListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnArticleSelectedListener");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_article, container, false);
+    }
+
+    public void onArticleBtnClicked(View view) {
+        mCallback.onArticleSelected(4);
+    }
+
+    public void updateArticleView(int position) {
+        TextView textView = (TextView) getView().findViewById(R.id.articleTitle);
+        textView.setText(position);
     }
 }
--- app/src/main/java/com/kapp/androidkapp/MainActivity.java
+++ app/src/main/java/com/kapp/androidkapp/MainActivity.java
@@ -1,12 +1,13 @@
 package com.kapp.androidkapp;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     public static final String EXTRA_MESSAGE = "com.kapp.androidkapp.MESSAGE";
 
