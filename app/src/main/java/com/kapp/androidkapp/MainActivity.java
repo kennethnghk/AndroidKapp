ee18e45f1287c80d5f0cf5b6eafbabddde53f998
Kenneth Ng <ng.tszcheong@gmail.com>
2017-03-19 21:40:37 +0800

    Add action bar

--- app/src/main/java/com/kapp/androidkapp/MainActivity.java
+++ app/src/main/java/com/kapp/androidkapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.kapp.androidkapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void sendMessage(View view) {
