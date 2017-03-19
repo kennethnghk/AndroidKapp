3af8c14f14cc6fa33d18fb1a1391f72e96c5cee1
Kenneth Ng <ng.tszcheong@gmail.com>
2017-03-19 17:29:14 +0800

    Pass arguments to fragment

--- app/src/main/java/com/kapp/androidkapp/ArticleFragment.java
+++ app/src/main/java/com/kapp/androidkapp/ArticleFragment.java
@@ -7,22 +7,42 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ArticleFragment extends Fragment {
 
+    String articleTitle;
+
     public static final String ARG_POSITION = "ARG_POSITION";
+
+    public static ArticleFragment newInstance(String articleTitle) {
+        ArticleFragment articleFragment = new ArticleFragment();
+        Bundle args = new Bundle();
+        args.putString("articleTitle", articleTitle);
+        articleFragment.setArguments(args);
+        return articleFragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_article, container, false);
+        Log.d("FRAG", articleTitle);
+
+        TextView articleTitleTextView = (TextView) view.findViewById(R.id.article_fragment_article_title);
+        articleTitleTextView.setText(articleTitle);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false);
+        return view;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("FRAG", "onCreate");
+
+        articleTitle = getArguments().getString("articleTitle", "");
+        Log.d("FRAG", articleTitle);
     }
     @Override
     public void onStart() {
--- app/src/main/java/com/kapp/androidkapp/ArticlesActivity.java
+++ app/src/main/java/com/kapp/androidkapp/ArticlesActivity.java
@@ -1,5 +1,6 @@
 package com.kapp.androidkapp;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class ArticlesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_articles);
+
+        if (findViewById(R.id.fragment_placeholder) != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ArticleFragment articleFragment = ArticleFragment.newInstance("my title");
+            ft.replace(R.id.fragment_placeholder, articleFragment);
+            ft.commit();
+        }
+
     }
 }
