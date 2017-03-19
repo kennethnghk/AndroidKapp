6c8017cdca14a376be8f99847e9677c317738732
Kenneth Ng <ng.tszcheong@gmail.com>
2017-03-19 16:30:41 +0800

    Include article fragment

--- app/src/main/java/com/kapp/androidkapp/ArticleFragment.java
+++ app/src/main/java/com/kapp/androidkapp/ArticleFragment.java
@@ -1,7 +1,9 @@
 package com.kapp.androidkapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,5 +17,56 @@ public class ArticleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_article, container, false);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("FRAG", "onCreate");
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d("FRAG", "onStart");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d("FRAG", "onResume");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("FRAG", "onPause");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d("FRAG", "onStop");
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d("FRAG", "onActivityCreated");
+    }
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d("FRAG", "onDetach");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("FRAG", "onDestroy");
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d("FRAG", "onDestroyView");
+    }
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        Log.d("FRAG", "onAttach");
     }
 }
--- app/src/main/java/com/kapp/androidkapp/ArticlesActivity.java
+++ app/src/main/java/com/kapp/androidkapp/ArticlesActivity.java
@@ -1,6 +1,5 @@
 package com.kapp.androidkapp;
 
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,35 +9,5 @@ public class ArticlesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_articles);
-
-        if (findViewById(R.id.fragment_container) != null) {
-
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
-                return;
-            }
-
-            Integer position = 1;
-
-            ArticleFragment firstFragment = new ArticleFragment();
-
-            Bundle args = new Bundle();
-            args.putInt(ArticleFragment.ARG_POSITION, position);
-            firstFragment.setArguments(args);
-
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
-//            firstFragment.setArguments(getIntent().getExtras());
-
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-//            transaction.add(R.id.fragment_container, firstFragment);
-            transaction.replace(R.id.fragment_container, firstFragment);
-            transaction.addToBackStack(null);
-
-            transaction.commit();
-        }
     }
 }
