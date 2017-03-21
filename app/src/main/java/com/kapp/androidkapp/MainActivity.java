fa497db684cd6c8a8d1d5141d07affaabdcfe63a
Kenneth Ng <ng.tszcheong@gmail.com>
2017-03-21 21:50:52 +0800

    Add action menu to toolbar

--- app/src/main/java/com/kapp/androidkapp/MainActivity.java
+++ app/src/main/java/com/kapp/androidkapp/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     public static final String EXTRA_MESSAGE = "com.kapp.androidkapp.MESSAGE";
 
     @Override
@@ -16,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
@@ -31,5 +33,13 @@ public class MainActivity extends AppCompatActivity {
     public void goToArticles(View view) {
         Intent intent = new Intent(this, ArticlesActivity.class);
         startActivity(intent);
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
